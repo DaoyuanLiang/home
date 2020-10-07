1e3348ef85c99ae7d52e5d8b959f5bcf9fd72b1b
xuejiaxin <@xuejiaxin>
2020-10-07 21:43:54 +0800

    测试上传

---
+++ precache-manifest.67e67ee76a3a09dc75caa7930c9a184e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aaec3e0940119d2d9af7aebb6f9883b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47729f611e13cdb659cb",
+    "url": "/static/css/2.3fe752fe.chunk.css"
+  },
+  {
+    "revision": "8086577c413c0129a42c",
+    "url": "/static/css/main.d694a8df.chunk.css"
+  },
+  {
+    "revision": "47729f611e13cdb659cb",
+    "url": "/static/js/2.c41e0e92.chunk.js"
+  },
+  {
+    "revision": "eb94d6017d8f0c633f4acf9346a3ecc8",
+    "url": "/static/js/2.c41e0e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8086577c413c0129a42c",
+    "url": "/static/js/main.bd917a89.chunk.js"
+  },
+  {
+    "revision": "71b326226918f1bd95d2",
+    "url": "/static/js/runtime-main.3c7650ac.js"
+  },
+  {
+    "revision": "a35a0498c088cc983a50dc5dd028ab54",
+    "url": "/static/media/stock-website-ico.a35a0498.ico"
+  }
+]);
